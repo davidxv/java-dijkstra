2d2589787b69b540bb4085315d230fbaffa2878d
Anders Nawroth <anders@neotechnology.com>
2010-05-31 01:23:47 +0200

    Some refactoring.

--- src/main/java/org/neo4j/examples/dijkstra/DijkstraExample.java
+++ src/main/java/org/neo4j/examples/dijkstra/DijkstraExample.java
@@ -1,26 +1,66 @@
 package org.neo4j.examples.dijkstra;
 
+import org.neo4j.graphalgo.PathFinder;
+import org.neo4j.graphalgo.path.Dijkstra;
 import org.neo4j.graphalgo.path.WeightedPath;
+import org.neo4j.graphalgo.util.DoubleEvaluator;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipExpander;
+import org.neo4j.kernel.TraversalFactory;
 
 public class DijkstraExample
 {
-    public static void main( final String[] args )
+    private final ExampleGraphService graph;
+
+    private static final String COST = "cost";
+    private static final RelationshipExpander expander;
+    private static final DoubleEvaluator costEvaluator;
+    private static final PathFinder<WeightedPath> dijkstraPathFinder;
+
+    static
     {
-        DijkstraService graph = new DijkstraService();
+        expander = TraversalFactory.expanderForTypes(
+                ExampleGraphService.MyDijkstraTypes.REL, Direction.BOTH );
+        costEvaluator = new DoubleEvaluator( COST );
+        dijkstraPathFinder = new Dijkstra( expander, costEvaluator );
+    }
 
-        graph.makeEdge( "s", "c", 7 );
-        graph.makeEdge( "c", "e", 7 );
-        graph.makeEdge( "s", "a", 2 );
-        graph.makeEdge( "a", "b", 7 );
-        graph.makeEdge( "b", "e", 2 );
+    public DijkstraExample()
+    {
+        graph = new ExampleGraphService();
+    }
 
-        WeightedPath path = graph.getDijkstra( 0.0, "s", "e" );
+    private void createGraph()
+    {
+        graph.makeEdge( "s", "c", COST, 7 );
+        graph.makeEdge( "c", "e", COST, 7 );
+        graph.makeEdge( "s", "a", COST, 2 );
+        graph.makeEdge( "a", "b", COST, 7 );
+        graph.makeEdge( "b", "e", COST, 2 );
+    }
+
+    private void runDijkstraPathFinder()
+    {
+        Node start = graph.getNode( "s" );
+        Node end = graph.getNode( "e" );
+        WeightedPath path = dijkstraPathFinder.findSinglePath( start, end );
         for ( Node node : path.nodes() )
         {
-            System.out.println( node.getProperty( DijkstraService.NAME ) );
+            System.out.println( node.getProperty( ExampleGraphService.NAME ) );
         }
+    }
 
+    private void shutdown()
+    {
         graph.shutdown();
+    }
+
+    public static void main( final String[] args )
+    {
+        DijkstraExample de = new DijkstraExample();
+        de.createGraph();
+        de.runDijkstraPathFinder();
+        de.shutdown();
     }
 }
--- src/main/java/org/neo4j/examples/dijkstra/ExampleGraphService.java
+++ src/main/java/org/neo4j/examples/dijkstra/ExampleGraphService.java
@@ -1,38 +1,27 @@
 package org.neo4j.examples.dijkstra;
 
-import org.neo4j.graphalgo.CostEvaluator;
-import org.neo4j.graphalgo.path.Dijkstra;
-import org.neo4j.graphalgo.path.WeightedPath;
-import org.neo4j.graphalgo.util.DoubleEvaluator;
-import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipExpander;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.index.IndexService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.kernel.TraversalFactory;
 import org.neo4j.util.GraphDatabaseLifecycle;
 
-public class DijkstraService
+public class ExampleGraphService
 {
-    private enum MyDijkstraTypes implements RelationshipType
+    public enum MyDijkstraTypes implements RelationshipType
     {
         REL
     }
 
     public static final String NAME = "name";
-    private static final String COST = "cost";
-    private final EmbeddedGraphDatabase graphDb;
-    private final IndexService index;
-    private final GraphDatabaseLifecycle lifecycle;
-    RelationshipExpander expander = TraversalFactory.expanderForTypes(
-            MyDijkstraTypes.REL, Direction.BOTH );
-    private final CostEvaluator<Double> costEvaluator = new DoubleEvaluator(
-            COST );
 
-    public DijkstraService()
+    private final EmbeddedGraphDatabase graphDb;
+    private final GraphDatabaseLifecycle lifecycle;
+    private final IndexService index;
+
+    public ExampleGraphService()
     {
         graphDb = new EmbeddedGraphDatabase( "target/neo4j-db" );
         lifecycle = new GraphDatabaseLifecycle( graphDb );
@@ -40,13 +29,9 @@ public class DijkstraService
         index = lifecycle.indexService();
     }
 
-    public void shutdown()
-    {
-        lifecycle.manualShutdown();
-    }
-
     public void makeEdge( final String firstNodeName,
-            final String secondNodeName, final double cost )
+            final String secondNodeName, final String propertyName,
+            final double cost )
     {
         Transaction tx = graphDb.beginTx();
 
@@ -59,7 +44,7 @@ public class DijkstraService
             // add relationship
             Relationship rel = firstNode.createRelationshipTo( secondNode,
                     MyDijkstraTypes.REL );
-            rel.setProperty( COST, cost );
+            rel.setProperty( propertyName, cost );
 
             tx.success();
         }
@@ -73,9 +58,14 @@ public class DijkstraService
         }
     }
 
+    public Node getNode( final String name )
+    {
+        return index.getSingleNode( NAME, name );
+    }
+
     private Node findOrCreateNode( final String nodeName )
     {
-        Node node = index.getSingleNode( NAME, nodeName );
+        Node node = getNode( nodeName );
         if ( node == null )
         {
             node = graphDb.createNode();
@@ -85,20 +75,8 @@ public class DijkstraService
         return node;
     }
 
-    public WeightedPath getDijkstra( final double startCost,
-            final String startNodeName, final String endNodeName )
+    public void shutdown()
     {
-        Node startNode = index.getSingleNode( NAME, startNodeName );
-        Node endNode = index.getSingleNode( NAME, endNodeName );
-        if ( startNode == null )
-        {
-            throw new IllegalArgumentException( "Start node not found." );
-        }
-        if ( endNode == null )
-        {
-            throw new IllegalArgumentException( "End node not found." );
-        }
-        Dijkstra dijkstra = new Dijkstra( expander, costEvaluator );
-        return dijkstra.findSinglePath( startNode, endNode );
+        lifecycle.manualShutdown();
     }
 }
